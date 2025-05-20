35d4911f9c680d2bc1a808e0b2ba0253236637fc
Anurag Kumar <138526681+anurag211824@users.noreply.github.com>
2025-05-20 21:14:58 +0530

    start backend and complete userRoute register,login and profile

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,85 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+// Registration Logic
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    //Registration
+    let user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "User already exists" });
+
+    user = new User({ name, email, password });
+    await user.save();
+    // Create JWT payload
+    const payload = { user: { id: user._id, role: user.role } };
+    // Sign and return the token along with user data
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+      (err, token) => {
+        if (err) throw err;
+
+        // Send the user and token in response
+        res.status(201).json({
+          user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+          },
+          token,
+        });
+      }
+    );
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Server error");
+  }
+};
+// Login logic
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    // Find the user by email
+    let user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid Credentials" });
+
+    // Create JWT payload
+    const payload = { user: { id: user._id, role: user.role } };
+    // Sign and return the token along with user data
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+      (err, token) => {
+        if (err) throw err;
+
+        // Send the user and token in response
+        res.json({
+          user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+          },
+          token,
+        });
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+};
+
+// User Profile Logic
+const userProfile = async (req, res) => {
+  res.json(req.user);
+};
+
+export { registerUser, loginUser, userProfile };
---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express"  // Importing the Express framework to create a web server
+import cors from "cors"        // Importing CORS middleware to allow cross-origin requests
+import dotenv from "dotenv"   // Importing dotenv to load environment variables from a .env file
+const app = express();         // Creating an instance of an Express application
+import connectDb from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js"
+app.use(express.json()) // Middleware to parse incoming JSON requests and make the data available in req.body
+app.use(cors()) // Middleware to enable Cross-Origin Resource Sharing (CORS), allowing frontend apps from different origins to access the backend
+dotenv.config()
+const PORT = process.env.PORT || 3000
+connectDb()
+
+app.get('/',(req,res)=>{
+    res.send("welcome to Rabbit")
+})
+//API Routes
+app.use('/api/users',userRoutes)
+app.listen(PORT,()=>{
+    console.log(`Sever is Running on ${PORT}`); 
+})
